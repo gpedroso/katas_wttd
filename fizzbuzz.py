d51502dbc5213b2418e3d8eecb48b4fdedfda3ea
Gabriel Pedroso <gpedroso@gmail.com>
2015-12-28 00:29:34 -0200

    assert_equal func

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -24,7 +24,7 @@ def multiple_of_3(num):
 def robot(pos):
     say = str(pos)
 
-    if multiple_of_5(pos) and multiple_of_3(pos):
+    if multiple_of_5(pos) or multiple_of_3(pos):
         say = 'fizzbuzz'
     elif multiple_of_5(pos):
         say = 'buzz'
@@ -32,19 +32,26 @@ def robot(pos):
         say = 'fizz'
     return say
 
+def assert_equal(first, second, line):
+    msg = "Fail: Line {} get {} expecting {}"
+    try:
+        assert first == second
+    except AssertionError:
+        print(msg.format(line, first,  second))
+
 if __name__ == '__main__':
-    assert robot(1) == '1'
-    assert robot(2) == '2'
-    assert robot(4) == '4'
+    assert_equal(robot(1), '1', 42)
+    assert_equal(robot(2), '2', 43)
+    assert_equal(robot(4), '4', 44)
 
-    assert robot(3) == 'fizz'
-    assert robot(6) == 'fizz'
-    assert robot(9) == 'fizz'
+    assert_equal(robot(3), 'fizz', 46)
+    assert_equal(robot(6), 'fizz', 47)
+    assert_equal(robot(9), 'fizz', 48)
 
-    assert robot(5) == 'buzz'
-    assert robot(10) == 'buzz'
-    assert robot(20) == 'buzz'
+    assert_equal(robot(5), 'buzz', 50)
+    assert_equal(robot(10), 'buzz', 51)
+    assert_equal(robot(20), 'buzz', 52)
 
-    assert robot(15) == 'fizzbuzz'
-    assert robot(30) == 'fizzbuzz'
-    assert robot(45) == 'fizzbuzz'
+    assert_equal(robot(15), 'fizzbuzz', 54)
+    assert_equal(robot(30), 'fizzbuzz', 55)
+    assert_equal(robot(45), 'fizzbuzz', 56)
