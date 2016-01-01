15dc63eab5e835b0ac2562017bf8b11193d4b52b
Gabriel Pedroso <gpedroso@gmail.com>
2016-01-01 10:42:22 -0200

    kata tests

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -24,34 +24,10 @@ def multiple_of_3(num):
 def robot(pos):
     say = str(pos)
 
-    if multiple_of_5(pos) or multiple_of_3(pos):
+    if multiple_of_5(pos) and multiple_of_3(pos):
         say = 'fizzbuzz'
     elif multiple_of_5(pos):
         say = 'buzz'
     elif multiple_of_3(pos):
         say = 'fizz'
     return say
-
-def assert_equal(first, second, line):
-    msg = "Fail: Line {} get {} expecting {}"
-    try:
-        assert first == second
-    except AssertionError:
-        print(msg.format(line, first,  second))
-
-if __name__ == '__main__':
-    assert_equal(robot(1), '1', 42)
-    assert_equal(robot(2), '2', 43)
-    assert_equal(robot(4), '4', 44)
-
-    assert_equal(robot(3), 'fizz', 46)
-    assert_equal(robot(6), 'fizz', 47)
-    assert_equal(robot(9), 'fizz', 48)
-
-    assert_equal(robot(5), 'buzz', 50)
-    assert_equal(robot(10), 'buzz', 51)
-    assert_equal(robot(20), 'buzz', 52)
-
-    assert_equal(robot(15), 'fizzbuzz', 54)
-    assert_equal(robot(30), 'fizzbuzz', 55)
-    assert_equal(robot(45), 'fizzbuzz', 56)
